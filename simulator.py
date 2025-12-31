80c5ae82506ee498595b48ec973ef840bf3389c1
TenHam3 <simbaandbella3@gmail.com>
2025-12-31 00:30:06 -0600

    Integrated pattern matrix generator from 3b1b into simulator and implemented entropy calculation based on pattern matrix

--- simulator.py
+++ simulator.py
@@ -6,35 +6,123 @@ a lookup table of results for fast access during solving
 import numpy as np
 import pandas as pd
 import math
+import json
+import time
+import itertools as it
+import os
 from wordle import *
 
+test_words = np.loadtxt('./data/test.txt', dtype=str)
 all_words = np.loadtxt('./data/allowed_words.txt', dtype=str)
 possible_words = np.loadtxt('./data/possible_words.txt', dtype=str)
 
 NUM_ALLOWED = len(all_words)
 NUM_POSSIBLE = len(possible_words)
 
-# Compute a pattern matrix that maps (target, guess) pairs to evaluation patterns
-
 def main():
+    # Get pattern matrix (check if already saved in file first)
+
+    if os.path.exists('./data/pattern_matrix.npy'):
+        pattern_matrix = np.load('./data/pattern_matrix.npy')
+    else:
+        pattern_matrix = generate_pattern_matrix(all_words, all_words)
+        np.save('./data/pattern_matrix.npy', pattern_matrix)
+
+    print(all_words[10170]) # slate
+    print(get_entropy(10170, pattern_matrix))
+
+    print(all_words[12358]) # weary
+    print(get_entropy(12358, pattern_matrix))
+
+    # Calculate expected information gain for each guess in allowed words
+
+    # Store results in a DataFrame and export to JSON for easy lookup during solving
+    # Maps guesses to expected info gain
+    
     return
 
 def safe_log(x):
     return math.log2(x) if x > 0 else 0
 
-def expected_info_gain():
+def words_to_int_arrays(words):
+    return np.array([[ord(c) for c in w] for w in words], dtype=np.uint8)
+
+
+def generate_pattern_matrix(words1, words2):
+    """
+    A pattern for two words represents the wordle-similarity
+    pattern (grey -> 0, yellow -> 1, green -> 2) but as an integer
+    between 0 and 3^5. Reading this integer in ternary gives the
+    associated pattern.
+
+    This function computes the pairwise patterns between two lists
+    of words, returning the result as a grid of hash values. Since
+    this can be time-consuming, many operations are vectorized
+    (perhaps at the expense of easier readibility), and the result
+    is saved to a file so that this only needs to be evaluated once, and
+    all remaining pattern matching is a lookup.
+    """
+
+    # Number of letters/words
+    nl = len(words1[0])
+    nw1 = len(words1)  # Number of words
+    nw2 = len(words2)  # Number of words
+
+    # Convert word lists to integer arrays
+    word_arr1, word_arr2 = map(words_to_int_arrays, (words1, words2))
+
+    # equality_grid keeps track of all equalities between all pairs
+    # of letters in words. Specifically, equality_grid[a, b, i, j]
+    # is true when words[a][i] == words[b][j]
+    equality_grid = np.zeros((nw1, nw2, nl, nl), dtype=bool)
+    for i, j in it.product(range(nl), range(nl)):
+        equality_grid[:, :, i, j] = np.equal.outer(word_arr1[:, i], word_arr2[:, j])
+
+    # full_pattern_matrix[a, b] should represent the 5-color pattern
+    # for guess a and answer b, with 0 -> grey, 1 -> yellow, 2 -> green
+    full_pattern_matrix = np.zeros((nw1, nw2, nl), dtype=np.uint8)
+
+    # Green pass
+    for i in range(nl):
+        matches = equality_grid[:, :, i, i].flatten()  # matches[a, b] is true when words[a][i] = words[b][i]
+        full_pattern_matrix[:, :, i].flat[matches] = EXACT
+
+        for k in range(nl):
+            # If it's a match, mark all elements associated with
+            # that letter, both from the guess and answer, as covered.
+            # That way, it won't trigger the yellow pass.
+            equality_grid[:, :, k, i].flat[matches] = False
+            equality_grid[:, :, i, k].flat[matches] = False
+
+    # Yellow pass
+    for i, j in it.product(range(nl), range(nl)):
+        matches = equality_grid[:, :, i, j].flatten()
+        full_pattern_matrix[:, :, i].flat[matches] = MISPLACED
+        for k in range(nl):
+            # Similar to above, we want to mark this letter
+            # as taken care of, both for answer and guess
+            equality_grid[:, :, k, j].flat[matches] = False
+            equality_grid[:, :, i, k].flat[matches] = False
+
+    # Rather than representing a color pattern as a lists of integers,
+    # store it as a single integer, whose ternary representations corresponds
+    # to that list of integers.
+    pattern_matrix = np.dot(
+        full_pattern_matrix,
+        (3**np.arange(nl)[::-1]).astype(np.uint8)
+    )
+
+    return pattern_matrix
+
+def get_entropy(guess, pattern_matrix):
     info_gain = 0.0
 
     # Iterate through each possible pattern and add its contribution to info gain
+    patterns, counts = np.unique(pattern_matrix[guess], return_counts=True)
+    probs = counts / counts.sum()
+    info_gain = -np.sum(probs * np.log2(probs))
 
     return info_gain
-
-# Calculate expected information gain for each guess in allowed words
-
-# Store results in a DataFrame and export to JSON for easy lookup during solving
-# Maps guesses to expected info gain
-
-
 
 if __name__ == "__main__":
     main()
